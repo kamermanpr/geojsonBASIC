5fbf4560848a22cc172703a182257e194c793b8f
Peter Kamerman <peter.kamerman@gmail.com>
2016-08-09 17:35:10 +0200

    Deleted all data/ and associated .R and .Rd files

--- R/admin0_countries_50m.R
+++
@@ -1,7 +0,0 @@
-#' 1:50m GeoJSON of admin level 0 detail (countries).
-#'
-#' A GeoJSON of admin level 0 detail (countries) scaled at 1:50m.
-#'
-#' @format GeoJSON
-#' @source \url{http://www.naturalearthdata.com/}
-"admin0_countries_110m"
